7ae820dfbf4275bb8cfc8a162c87f56edff38298
dannyarena <arenadanny71@gmail.com>
2025-05-28 21:03:56 +0200

    add second array of actress

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,29 @@ import './App.css'
 
 function App() {
 const [persone, setPersone] = useState([]);
-const [attoreCercato, setAttoreCercato] = useState("");
+const [nomeCercato, setNomeCercato] = useState("");
 
-useEffect (() => {
+useEffect(() => {
+  // prima gli attori
   fetch('https://lanciweb.github.io/demo/api/actors/')  // chiamata
-  .then(response => response.json())  //traduzione in JSON
-  .then(data => {
-    console.log(data);  //stampa
-    setPersone(data);  //salvataggio nello stato
-    
-    
-  }); }, []
-);
-    
+    .then(response => response.json())  //traduzione in JSON
+    .then(dataAttori => {
+       // poi le attrici
+      fetch('https://lanciweb.github.io/demo/api/actresses/')  // chiamata
+        .then(response => response.json())  //traduzione in JSON
+        .then(dataAttrici => {
+        //  poi unisci
+          const unione = [...dataAttori, ...dataAttrici];
+          setPersone(unione);
+        });
+    }); }, []);
   
 return (
   <>
     <input type="text"
     placeholder='Cerca attore'
-    value={attoreCercato}
-    onChange={(e) => setAttoreCercato(e.target.value)}
+    value={nomeCercato}
+    onChange={(e) => setNomeCercato(e.target.value)}
     />
 
    <div>
@@ -31,7 +34,7 @@ return (
     {/* creiamo le card dinamiche con il .map */}
     <div className='card-container'>
       {/* filtro per nome */}
-    {persone.filter(p=> p.name.toLowerCase().includes(attoreCercato.toLowerCase()))
+    {persone.filter(p => p.name.toLowerCase().includes(nomeCercato.toLowerCase()))
     .map(p => (
       <div className='card' key={p.name}>
           <img src={p.image} alt={p.name} />
