9c33d21ad45d6dc8c5bb56cc31b7c8fe485d5636
dannyarena <arenadanny71@gmail.com>
2025-05-28 16:22:03 +0200

    add fetch to get data from endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+const [persone, setPersone] = useState([])
+
+useEffect (() => {
+  fetch('https://lanciweb.github.io/demo/api/actors/')  // chiamata
+  .then(response => response.json())  //traduzione in JSON
+  .then(data => {
+    console.log(data);  //stampa
+    setPersone(data);  //salvataggio nello stato
+  });
+}, []);
 
   return (
-   <div></div>
+   <div>
+    <h1>Lista di attori</h1>
+   </div>
   )
 }
 
